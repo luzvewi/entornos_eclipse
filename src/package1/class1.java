54915a10ef0d06434a6b78b4a969d7d833df7820
luzvewi <116590899+luzvewi@users.noreply.github.com>
2023-02-09 20:47:15 +0100

    cuarta interacción

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -1,9 +1,24 @@
 package package1;
 
+import java.util.Scanner;
+
 public class class1 {
 public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	boolean boolean1=false;
+	boolean boolean2=false;
+	
 	String patata="hola soy una patata";
 	String tomate="hola soy un tomate";
 	System.out.println(patata+tomate);
+	System.out.println("inserta una fruta");
+	String fruta= sc.next();
+	
+	if(fruta.equals(patata))
+		boolean1=true;
+	
+	if(fruta.equals(tomate))
+		boolean2=true;
+	System.out.println("la fruta es una patata? "+boolean1+" la fruta es un tomate? "+boolean2);
 }
 }
