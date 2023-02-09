7d846d87405f114b8de8e1dd20c74205bd921cf9
luzvewi <116590899+luzvewi@users.noreply.github.com>
2023-02-09 20:38:04 +0100

    tercera interacción

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -1,5 +1,9 @@
 package package1;
 
 public class class1 {
-
+public static void main(String[] args) {
+	String patata="hola soy una patata";
+	String tomate="hola soy un tomate";
+	System.out.println(patata+tomate);
+}
 }
